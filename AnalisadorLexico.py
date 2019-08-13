b20f8217d065b570048df97ad4a781a2ee4e4f33
erick <erick.costa@contabilivre.com.br>
2019-08-12 23:01:14 -0300

    lendo arquivos

--- AnalisadorLexico.py
+++ AnalisadorLexico.py
@@ -1,24 +1,10 @@
 #ler arquivo TXT
-class Texto(object): 
-  def __init__(self, conteudo): 
-    self.__conteudo = conteudo
+arquivo = open('teste.txt', 'r')
 
-  @property 
-  def conteudo(self): 
-    return self.__conteudo
-
-  @nome.setter 
-  def nome(self, conteudo): 
-    self.__conteudo = conteudo
-
-  def __repr__(self): 
-    return "%s" % (self.__conteudo)
-
-  def ler_produtos(arquivo):
-    arquivo = 'arquivos/code.txt'
-
-  def ler_produtos(arquivo):
-    arquivo_aberto = open(arquivo, 'rb')
-
+conteudo = arquivo.readlines()
+teste = 'teste;teste2;teste3'
+print(teste.split(";",2))
+for
+print(conteudo)
 
 
